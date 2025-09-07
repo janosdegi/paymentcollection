456b622609b8b6c055fbfaed1aa5423cc6e17176
degijanos <johann.degi@gmail.com>
2025-09-07 15:24:40 +0200

    Add new config to tests

--- src/test/java/io/paymentcollection/PaymentCollectionApplicationTests.java
+++ src/test/java/io/paymentcollection/PaymentCollectionApplicationTests.java
@@ -2,10 +2,14 @@ package io.paymentcollection;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
-@ActiveProfiles("test")
+@SpringBootTest(properties = {
+        "spring.main.web-application-type=none",
+        "spring.autoconfigure.exclude="
+                + "org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration,"
+                + "org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration,"
+                + "org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration"
+})
 class PaymentCollectionApplicationTests {
 
   @Test
