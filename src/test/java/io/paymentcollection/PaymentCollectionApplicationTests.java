995ce9386e2d275acb92a5be8cf5b26cc7525131
degijanos <johann.degi@gmail.com>
2025-09-07 15:44:50 +0200

    solving format violation issues

--- src/test/java/io/paymentcollection/PaymentCollectionApplicationTests.java
+++ src/test/java/io/paymentcollection/PaymentCollectionApplicationTests.java
@@ -3,13 +3,14 @@ package io.paymentcollection;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(properties = {
-        "spring.main.web-application-type=none",
-        "spring.autoconfigure.exclude="
-                + "org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration,"
-                + "org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration,"
-                + "org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration"
-})
+@SpringBootTest(
+    properties = {
+      "spring.main.web-application-type=none",
+      "spring.autoconfigure.exclude="
+          + "org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration,"
+          + "org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration,"
+          + "org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration"
+    })
 class PaymentCollectionApplicationTests {
 
   @Test
